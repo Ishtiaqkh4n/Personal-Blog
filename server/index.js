4fedb889739104887024327ecf8cab3859f5be96
ishtiaq khan <ishtiaqkhan@debian.debian.com>
2026-03-19 01:17:40 -0400

    BAsic authentication method added

--- server/index.js
+++ server/index.js
@@ -2,13 +2,22 @@ import express from "express"
 import cors from "cors"
 import fs from "fs"
 const filePath = "./store.json"
+import basicAuth from "express-basic-auth"
 
 
 const app = express()
 const PORT = 8000;
 
 
-app.use(cors())
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json())
 
 
@@ -30,6 +39,17 @@ app.get("/getData",(req,res)=>{
 })
 
 
+const middleAuth = basicAuth({
+    users:{
+        "admin":"admin"
+    },
+    challenge:true,
+    unauthorizedResponse:(req)=>{
+        return `Acces Denied : Invalid Credentials`
+    }
+
+})
+
 app.get("/article/:Id",(req,res)=>{
     const {Id} = req.params;
     const data = loadData()
@@ -40,6 +60,21 @@ app.get("/article/:Id",(req,res)=>{
        .json(article)
     }
     })
+})
+
+
+//middleware 
+app.use("/auth",middleAuth)
+
+// app.get("/admin",(req,res)=>{
+//     res.status(200).send("HELLO THIS IS DASHBOARD")
+// })
+
+app.get("/auth/admin",(req,res)=>{
+     console.log("clicked")
+     res
+    .status(200)
+    .send(loadData())
 })
 
 
